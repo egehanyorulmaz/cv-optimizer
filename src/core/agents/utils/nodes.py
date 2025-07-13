aba6becf180381d08d374bbdd6470f98f2c98fdf
Egehan Yorulmaz <egehanyorulmaz@gmail.com>
2025-07-13 16:23:58 -0500

    Enhance search_company_info_node to support multiple branches for company information retrieval and update return type to include CompanyInfo

--- src/core/agents/utils/nodes.py
+++ src/core/agents/utils/nodes.py
@@ -1,14 +1,18 @@
 import asyncio
-from typing import Dict, Optional
+from typing import Dict, Optional, Literal, List
 from langsmith import traceable
+import logging
+
 from src.core.agents.utils.state import AgentState
 from src.core.domain.resume import Resume
 from src.core.domain.job_description import JobDescription
-from src.core.domain.company_search import CompanySearchResponse
+from src.core.domain.company_search import CompanyInfo
 from src.infrastructure.components import llm_extractor
 from src.core.ports.secondary.llm_extractor import LLMExtractor
 from src.core.ports.secondary.template_service import TemplateService
 from src.core.agents.search_agents import search_company_info
+
+logger = logging.getLogger("core.agents.nodes")
 
 @traceable(run_type="llm")
 async def parse_resume_node(state: AgentState, extractor: LLMExtractor):
@@ -33,14 +37,20 @@ async def parse_job_description_node(state: AgentState, extractor: LLMExtractor)
 @traceable(run_type="tool")
 async def search_company_info_node(state: AgentState, 
                                    template_service: TemplateService, 
-                                   model_name="gpt-4o") -> Dict[str, Optional[CompanySearchResponse]]:
-    """
-    Search for information about a company and return structured data.
-    """
-    company_names = [experience.company for experience in state["resume"].experiences]
+                                   branch: Literal["resume", "job_desription"] = "job_description",
+                                   model_name="gpt-4.1-mini") -> Dict[str, Optional[CompanyInfo]]:
+    if branch == "resume":
+        companies = state["resume"].company_names
+    elif branch == "job_description":
+        companies = [state["job_description"].company_name]
+    else:
+        raise ValueError("This branch is not supported.")
+    
+    tasks = [search_company_info(company_name=company_name, 
+                                 model_name=model_name, 
+                                 template_service=template_service) for company_name in companies]
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    results = {result.name: result for result in results if isinstance(result, CompanyInfo)}
+    logger.info(f"Retrieved information for branch {branch} with length {len(results)} companies.")
 
-    tasks = [search_company_info(company, template_service, model_name) for company in company_names]
-    results = await asyncio.gather(*tasks)
-    company_info_list = [result for result in results if result is not None]
-    print(f"Retrieved information for {len(company_info_list)} out of {len(company_names)} companies.")
-    return {"company_info": company_info_list}
+    return {f"{branch}_company_info": results}
